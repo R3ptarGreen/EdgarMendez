2886fad6a5fd2254f62dc288f22a1e2fdb65767c
Edgar <jorge260590@hotmail.com>
2023-12-31 00:40:42 -0500

    ci(AppRouter, <ScrollTop/>): Add a default redirection in react-router-dom

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,26 @@
 import { HomePage, BiographyPage, ProjectsPage } from '../pages/index';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider, useLocation } from 'react-router-dom';
 import App from '../App';
+import { useEffect } from 'react';
 
+const ScrollTop = () => {
+	
+	const {pathname} = useLocation()
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	},[pathname])
+}
 const AppRouter = () => {
 	const router = createHashRouter([
 		{
 			path: '/',
-			element: <App />,
+			element: (
+				<>
+					<App/>
+					<ScrollTop/>
+				</>
+			),
 			children: [
 				{ path: '/', element: <HomePage /> },
 				{ path: '/biography', element: <BiographyPage /> },
